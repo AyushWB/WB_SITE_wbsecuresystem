7772550a152c4a7847a820986f082ea6307c6085
Yash Singh <iamyashsinghh@gmail.com>
2024-12-09 13:07:21 +0530

    ok commit

--- pages/api/blog_sitmap/index.js
+++ pages/api/blog_sitmap/index.js
@@ -2,19 +2,19 @@ import fs from 'fs';
 import path from 'path';
 
 export default async function handler(req, res) {
-    try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/api/blog_sitmap`);
-        const blogs = await response.json();
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/api/blog_sitmap`);
+    const blogs = await response.json();
 
-        if (!Array.isArray(blogs)) {
-            throw new Error('Expected an array of blogs from the API response');
-        }
+    if (!Array.isArray(blogs)) {
+      throw new Error('Expected an array of blogs from the API response');
+    }
 
-        const baseUrl = 'https://weddingbanquets.in/blog';
-        const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
+    const baseUrl = 'https://weddingbanquets.in/blog';
+    const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${blogs
-                .map(blog => `
+        .map(blog => `
     <url>
       <loc>${baseUrl}/${blog.slug}</loc>
       <lastmod>${new Date().toISOString()}</lastmod>
@@ -22,16 +22,16 @@ export default async function handler(req, res) {
       <priority>0.9</priority>
     </url>
   `)
-                .join('')}
+        .join('')}
 </urlset>`;
 
-        // Save the sitemap in the public directory
-        const filePath = path.join(process.cwd(), 'public', 'blog_sitemap.xml');
-        fs.writeFileSync(filePath, sitemapContent);
+    // Save the sitemap in the public directory
+    const filePath = path.join(process.cwd(), 'public', 'blog_sitemap.xml');
+    fs.writeFileSync(filePath, sitemapContent);
 
-        res.status(200).json({ message: 'Sitemap generated successfully!' });
-    } catch (error) {
-        console.error('Error generating sitemap:', error);
-        res.status(500).json({ error: 'Failed to generate sitemap' });
-    }
+    res.status(200).json({ message: 'Sitemap generated successfully!' });
+  } catch (error) {
+    console.error('Error generating sitemap:', error);
+    res.status(500).json({ error: 'Failed to generate sitemap' });
+  }
 }
---
+++ pages/api/get_sitemap/sitemap_location_venues.js
@@ -0,0 +1,44 @@
+export default async function handler(req, res) {
+  const fetchPages = async () => {
+    const response = await fetch('https://cms.weddingbanquets.in/api/sitemap_location_venues/1');
+    const data = await response.json();
+    return data.sitemap.map((page) => ({
+      loc: `${process.env.SITE_URL || 'https://weddingbanquets.in'}/${page.url}`,
+      lastmod: new Date().toISOString(),
+      images: page.images || [],
+    }));
+  };
+
+  const generateSitemap = async () => {
+    const pages = await fetchPages();
+    let sitemapXML = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n`;
+    pages.forEach((page) => {
+      sitemapXML += `  <url>\n`;
+      sitemapXML += `    <loc>${page.loc}</loc>\n`;
+      sitemapXML += `    <lastmod>${page.lastmod}</lastmod>\n`;
+
+      if (page.images.length > 0) {
+        page.images.forEach((image) => {
+          sitemapXML += `    <image:image>\n`;
+          sitemapXML += `      <image:loc>${image.loc}</image:loc>\n`;
+          if (image.title) {
+            sitemapXML += `      <image:title>${image.title}</image:title>\n`;
+          }
+          if (image.caption) {
+            sitemapXML += `      <image:caption>${image.caption}</image:caption>\n`;
+          }
+          sitemapXML += `    </image:image>\n`;
+        });
+      }
+
+      sitemapXML += `  </url>\n`;
+    });
+
+    sitemapXML += `</urlset>\n`;
+
+    return sitemapXML;
+  };
+
+  const sitemap = await generateSitemap();
+  res.status(200).send(sitemap);
+}
