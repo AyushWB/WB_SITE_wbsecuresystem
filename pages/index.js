17ba240934c1f38e29f2e5608d0f2f974e38df56
Yash Singh <iamyashsinghh@gmail.com>
2025-03-01 13:10:52 +0530

    OK COMMIT

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,8 @@ import LeadForm from "@/components/homepage/leadform/LeadForm";
 import CityVenueHall from "@/components/miscellaneous/footer/CityVenueHall";
 import VendorCategoryCardGrid from "@/components/miscellaneous/vendorcategorycardgrid/VendorCategoryCardGrid";
 import Head from "next/head";
+
+const FB_PIXEL_ID = "852285655467761";
 export default function Home({
   venueCategogies,
   cities,
@@ -77,7 +79,14 @@ export default function Home({
       },
     ],
   };
-  
+
+  useEffect(() => {
+    window.fbq = window.fbq || function () {
+      (window.fbq.q = window.fbq.q || []).push(arguments);
+    };
+    window.fbq("init", FB_PIXEL_ID);
+    window.fbq("track", "PageView");
+  }, []);
 
   return (
     <div>
@@ -89,6 +98,32 @@ export default function Home({
         <meta property="og:url" content="https://weddingbanquets.in" />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}></script>
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdData) }}></script>
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
       </Head>
       <Navbar />
       <Hero
@@ -116,7 +151,7 @@ export async function getStaticProps() {
     let homePageData = await fetch(url);
     homePageData = await homePageData.json();
 
-    let  blogposts = homePageData.data.blogs;
+    let blogposts = homePageData.data.blogs;
 
     return {
       props: {
