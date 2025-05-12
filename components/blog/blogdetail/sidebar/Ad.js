bbab4cbd2a82f39e1e42bbdf7fa7050bf1322ac2
Yash Singh <iamyashsinghh@gmail.com>
2025-05-12 14:59:03 +0530

    ok

--- components/blog/blogdetail/sidebar/Ad.js
+++ components/blog/blogdetail/sidebar/Ad.js
@@ -1,111 +1,115 @@
-import { useGlobalContext } from '@/context/MyContext';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import React from 'react'
-import { IoIosCall } from 'react-icons/io';
-import styled from 'styled-components';
+import { useGlobalContext } from "@/context/MyContext";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React from "react";
+import { IoIosCall } from "react-icons/io";
+import styled from "styled-components";
 
 function Ad() {
-    const router = useRouter();
-    const { setLeadFormData, setIsLeadsModelOpen } = useGlobalContext();
+  const router = useRouter();
+  const { setLeadFormData, setIsLeadsModelOpen } = useGlobalContext();
 
-    const openLeadsModel = (e, type = 'click') => {
-        if (e) e.stopPropagation();
+  const openLeadsModel = (e, type = "click") => {
+    if (e) e.stopPropagation();
 
-        const pathSegments = router.asPath.split('/').filter((segment) => segment.length > 0);
-        let venue_slug = pathSegments.length === 2 ? pathSegments[1] : router.asPath;
+    const pathSegments = router.asPath
+      .split("/")
+      .filter((segment) => segment.length > 0);
+    let venue_slug =
+      pathSegments.length === 2 ? pathSegments[1] : router.asPath;
 
-        const leadData = {
-            url: router.asPath,
-            venue_slug: venue_slug,
-            type: type,
-            request_handle_by: 'form',
-        };
-
-        setLeadFormData(leadData);
-        setIsLeadsModelOpen(true);
+    const leadData = {
+      url: router.asPath,
+      venue_slug: venue_slug,
+      type: type,
+      request_handle_by: "form",
     };
+
+    setLeadFormData(leadData);
+    setIsLeadsModelOpen(true);
+  };
   return (
     <Wrapper>
-       <div className="action-btns">
-            <button className="venue-card-btn" onClick={(e) => openLeadsModel(e)}>Get Quotation</button>
-            <span className="call-btn">
-                <a href={`tel:07969071909`} aria-label="call icon">
-                    <IoIosCall className="call-icon" size={30} />
-                </a>
-            </span>
-        </div>
-        <div className="bannar-img">
-            <Image
-                src="/common/venue.jpg"
-                alt="An example image"
-                fill={true}
-                />
-        </div>
+      <div className="action-btns">
+        <button className="venue-card-btn" onClick={(e) => openLeadsModel(e)}>
+          Get Quotation
+        </button>
+        <span className="call-btn">
+          <a href={`tel:07969071909`} aria-label="call icon">
+            <IoIosCall className="call-icon" size={30} />
+          </a>
+        </span>
+      </div>
+      <div className="bannar-img">
+        <img
+          src="/common/venue.jpg"
+          alt="An example image"
+          style={{ width: "100%", height: "auto" }}
+        />
+      </div>
     </Wrapper>
-  )
+  );
 }
 
-export default Ad
+export default Ad;
 
 const Wrapper = styled.div`
-background-color: white;
-margin-top: 2rem;
-position: sticky;
-top: 100px;
-z-index: 1;
+  background-color: white;
+  margin-top: 2rem;
+  position: sticky;
+  top: 100px;
+  z-index: 1;
 
-    .action-btns{
-        padding: 2rem 3rem;
-        display: flex;
-        justify-content: space-between;
+  .action-btns {
+    padding: 2rem 3rem;
+    display: flex;
+    justify-content: space-between;
 
-        .venue-card-btn{
-            border: none;
-            white-space: nowrap;
-            background:none;
-            border: 1px solid #F33232;
-            color: #F33232;
-            padding: 1rem 2.5rem;
-            text-transform: uppercase;
-            border-radius:.5rem;
-            font-size: 2rem;
-            cursor: pointer;
-            transition: all .3s linear;
-            &:hover{
-                background: #F33232;
-                color: white;
-            }
-        }
-       
-        .call-btn{
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            border: 1px solid var(--phone);  
-            padding:  0rem 1rem;
-            border-radius: .5rem;
-            cursor: pointer;
-            transition: all .2s linear;
-            .call-icon{
-                color: var(--phone);
-            }
-            
-            &:hover{
-                background-color: var(--phone);
-                border: 1px solid white;
-                .call-icon{
-                    color: white;
-                }
-            }
-        }
+    .venue-card-btn {
+      border: none;
+      white-space: nowrap;
+      background: none;
+      border: 1px solid #f33232;
+      color: #f33232;
+      padding: 1rem 2.5rem;
+      text-transform: uppercase;
+      border-radius: 0.5rem;
+      font-size: 2rem;
+      cursor: pointer;
+      transition: all 0.3s linear;
+      &:hover {
+        background: #f33232;
+        color: white;
+      }
     }
 
-    .bannar-img{
-        position: relative;
-        width: 100%;
-        height: 400px;
-        z-index: 0;
-    }
+    .call-btn {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border: 1px solid var(--phone);
+      padding: 0rem 1rem;
+      border-radius: 0.5rem;
+      cursor: pointer;
+      transition: all 0.2s linear;
+      .call-icon {
+        color: var(--phone);
+      }
 
+      &:hover {
+        background-color: var(--phone);
+        border: 1px solid white;
+        .call-icon {
+          color: white;
+        }
+      }
+    }
+  }
+
+  .bannar-img {
+    position: relative;
+    width: 100%;
+    height: 400px;
+    z-index: 0;
+  }
 `;
--- components/venue/venuedetailspage/venuebasicinfo/PriceListCard.js
+++ components/venue/venuedetailspage/venuebasicinfo/PriceListCard.js
@@ -1,192 +1,195 @@
 import styled from "styled-components";
 import Veg from "@/components/miscellaneous/Veg";
-import { IoIosCall } from 'react-icons/io'
+import { IoIosCall } from "react-icons/io";
 import Image from "next/image";
 import CallingRequest from "@/lib/request/callingrequest/CallingRequest";
 import useCallConversion from "@/lib/hook/useCallConversion";
 
+export default function PriceListCard({
+  min_capacity,
+  max_capacity,
+  nonveg_price,
+  veg_price,
+  phone,
+  slug,
+  openLeadsModel,
+  id,
+}) {
+  const { callConversion } = useCallConversion();
 
-
-export default function PriceListCard({ min_capacity, max_capacity, nonveg_price, veg_price, phone, slug, openLeadsModel, id }) {
-
-
-    const { callConversion } = useCallConversion();
-
-
-
-
-    async function handleAnchorClick(e, slug) {
-        // e.stopPropagation();
-        await CallingRequest(slug);
-
-    }
-    return (<Wrapper className="venue-price-list-card">
-        <div className="details">
-            <div className="detail">
-                <div className="detail-title">
-                    <Image
-                        // className="icon"
-                        src="/icons/team.png"
-                        alt="An example image"
-                        width={"20"}
-                        height={"20"}
-                    />
-                    <p className="desc">Capacity: </p>
-                </div>
-                <p className="desc bold">
-                    {min_capacity || ""} to {max_capacity || ""}
-                </p>
-            </div>
-            <div className="detail">
-                <div className="detail-title">
-                    <span className="icon">
-                        <Veg color={"green"} />
-                    </span>
-                    <p className="desc">
-                        Vegetarian:
-                    </p>
-                </div>
-                <div className="detail-content">
-                    <p className="desc bold">
-                    {veg_price ? (<del>{veg_price}</del>) : ("On Demand")}/plate
-                    </p>
-                </div>
-            </div>
-            {(nonveg_price && nonveg_price !== null && nonveg_price > 0) ? (
-                <div className="detail">
-                    <div className="detail-title">
-                        <span className="icon">
-                            <Veg color={"red"} />
-                        </span>
-                        <p className="desc">
-                            Non Vegetarian:
-                        </p>
-                    </div>
-                    <div className="detail-content">
-                        <p className="desc bold">
-                            <del>{nonveg_price}</del>/plate
-                        </p>
-                    </div>
-                </div>
-            ) : ('')}
-
-
-
-        </div>
-        <div className="action-btns">
-            <button className="venue-card-btn" onClick={() => { openLeadsModel() }}>Get Quotation</button>
-
-            <span className="call-btn">
-                <a href={`tel:0${phone}`} onClick={(e) => { handleAnchorClick(e, slug); callConversion(e, slug, id) }} aria-label="call icon ">
-                    <IoIosCall className="call-icon" size={30} />
-                </a>
-            </span>
-        </div>
-        <div className="bannar-img">
+  async function handleAnchorClick(e, slug) {
+    // e.stopPropagation();
+    await CallingRequest(slug);
+  }
+  return (
+    <Wrapper className="venue-price-list-card">
+      <div className="details">
+        <div className="detail">
+          <div className="detail-title">
             <Image
-                src="/common/venue.jpg"
-                alt="An example image"
-                fill={true}
+              // className="icon"
+              src="/icons/team.png"
+              alt="An example image"
+              width={"20"}
+              height={"20"}
             />
+            <p className="desc">Capacity: </p>
+          </div>
+          <p className="desc bold">
+            {min_capacity || ""} to {max_capacity || ""}
+          </p>
         </div>
-    </Wrapper>)
+        <div className="detail">
+          <div className="detail-title">
+            <span className="icon">
+              <Veg color={"green"} />
+            </span>
+            <p className="desc">Vegetarian:</p>
+          </div>
+          <div className="detail-content">
+            <p className="desc bold">
+              {veg_price ? <del>{veg_price}</del> : "On Demand"}/plate
+            </p>
+          </div>
+        </div>
+        {nonveg_price && nonveg_price !== null && nonveg_price > 0 ? (
+          <div className="detail">
+            <div className="detail-title">
+              <span className="icon">
+                <Veg color={"red"} />
+              </span>
+              <p className="desc">Non Vegetarian:</p>
+            </div>
+            <div className="detail-content">
+              <p className="desc bold">
+                <del>{nonveg_price}</del>/plate
+              </p>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+      <div className="action-btns">
+        <button
+          className="venue-card-btn"
+          onClick={() => {
+            openLeadsModel();
+          }}
+        >
+          Get Quotation
+        </button>
+
+        <span className="call-btn">
+          <a
+            href={`tel:0${phone}`}
+            onClick={(e) => {
+              handleAnchorClick(e, slug);
+              callConversion(e, slug, id);
+            }}
+            aria-label="call icon "
+          >
+            <IoIosCall className="call-icon" size={30} />
+          </a>
+        </span>
+      </div>
+      <div className="bannar-img">
+        <img
+          src="/common/venue.jpg"
+          alt="An example image"
+          style={{ width: "100%", height: "auto" }}
+        />
+      </div>
+    </Wrapper>
+  );
 }
 
+const Wrapper = styled.div`
+  background-color: white;
+  position: sticky;
+  top: 0;
+  .details {
+    padding: 2rem 3rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
 
-const Wrapper = styled.div`    
-    background-color: white;
-    position: sticky;
-    top: 0;
-    .details{
-        padding: 2rem 3rem;
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-        
-
-        .detail{
-            display: flex;
-            /* align-items: center; */
-            justify-content: space-between;
-        }
-        .detail-title{
-            display: flex;
-            gap: .5rem;
-        }
-        .icon{
-            position: relative;
-            width: 20px;
-            height: 20px;
-            display: flex;
-            gap: 1rem;  
-        }
-        
-        .desc{
-            /* color: var(--para)       ; */
-            font-family: 'Poppins';
-            font-size: 1.7rem;
-        
-        }
-        .bold{
-            font-weight:bold;   
-        }
-
+    .detail {
+      display: flex;
+      /* align-items: center; */
+      justify-content: space-between;
+    }
+    .detail-title {
+      display: flex;
+      gap: 0.5rem;
+    }
+    .icon {
+      position: relative;
+      width: 20px;
+      height: 20px;
+      display: flex;
+      gap: 1rem;
     }
 
-    .action-btns{
-        padding: 2rem 3rem;
-        display: flex;
-        justify-content: space-between;
-        .venue-card-btn{
-            border: none;
-            white-space: nowrap;
-            background:none;
-            /* box-sizing: border-box; */
-            border: 1px solid #F33232;
-            color: #F33232;
-            padding: 1rem 2.5rem;
-            text-transform: uppercase;
-            border-radius:.5rem;
-            font-size: 2rem;
-            cursor: pointer;
-            transition: all .3s linear;
-            &:hover{
-                background: #F33232;
-                color: white;
-            }
-        }
-        .call-btn{
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            border: 1px solid var(--phone);
-            padding:  0rem 1rem;
-            border-radius: .5rem;
-            cursor: pointer;
-            transition: all .2s linear;
-            .call-icon{
-
-                color: var(--phone);
-            }
-
-            &:hover{
-                background-color: var(--phone);
-                border: 1px solid white;
-
-                .call-icon{
-                    color: white;
-                }
-            }
-
-        }
-
+    .desc {
+      /* color: var(--para)       ; */
+      font-family: "Poppins";
+      font-size: 1.7rem;
     }
-
-    .bannar-img{
-        position: relative;
-        width: 100%;
-        height: 400px;
+    .bold {
+      font-weight: bold;
     }
+  }
 
+  .action-btns {
+    padding: 2rem 3rem;
+    display: flex;
+    justify-content: space-between;
+    .venue-card-btn {
+      border: none;
+      white-space: nowrap;
+      background: none;
+      /* box-sizing: border-box; */
+      border: 1px solid #f33232;
+      color: #f33232;
+      padding: 1rem 2.5rem;
+      text-transform: uppercase;
+      border-radius: 0.5rem;
+      font-size: 2rem;
+      cursor: pointer;
+      transition: all 0.3s linear;
+      &:hover {
+        background: #f33232;
+        color: white;
+      }
+    }
+    .call-btn {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border: 1px solid var(--phone);
+      padding: 0rem 1rem;
+      border-radius: 0.5rem;
+      cursor: pointer;
+      transition: all 0.2s linear;
+      .call-icon {
+        color: var(--phone);
+      }
 
-`
+      &:hover {
+        background-color: var(--phone);
+        border: 1px solid white;
+
+        .call-icon {
+          color: white;
+        }
+      }
+    }
+  }
+
+  .bannar-img {
+    position: relative;
+    width: 100%;
+    height: 400px;
+  }
+`;
