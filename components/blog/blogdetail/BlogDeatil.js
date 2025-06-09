f5e5303c417ee9a304302d0a72430d31c10ecbae
Ayush <ayush02.wb@gmail.com>
2025-06-09 16:29:22 +0530

    ok

--- components/blog/blogdetail/BlogDeatil.js
+++ components/blog/blogdetail/BlogDeatil.js
@@ -1,147 +1,3 @@
-// import React from "react";
-// import Head from "next/head";
-// import styled from "styled-components";
-// import FetureImg from "./description/FetureImg";
-// import PopularPost from "./sidebar/PopularPost";
-// import LatestPost from "./sidebar/LatestPost";
-// import Ad from "./sidebar/Ad";
-// import AuthorCard from "./authorcard/AuthorCard";
-
-// const BlogDetail = ({ data }) => {
-//   const blog = data.data;
-//   const popular = data.popular;
-//   const latest = data.latest;
-//   const author = data.author;
-
-//   const structuredData = {
-//     "@context": "https://schema.org",
-//     "@type": "Article",
-//     "headline": blog.heading,
-//     "description": blog.meta_description, 
-//     "datePublished": `${blog.publish_date}`,
-//     "mainEntityOfPage": {
-//       "@type": "WebPage",
-//       "@id": blog.slug 
-//     },
-//     "image": {
-//       "@type": "ImageObject",
-//       "url": `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/${blog.image}`,
-//       "width": 823,
-//       "height": 548
-//     },
-//     "author": {
-//       "@type": "Person",
-//       "name": author.name,
-//     },
-//     "publisher": {
-//       "@type": "Organization",
-//       "name": "www.weddingbanquets.in",
-//       "logo": {
-//         "@type": "ImageObject",
-//         "url": "https://www.weddingbanquets.in/logo.png", 
-//         "width": 280,
-//         "height": 60
-//       }
-//     }
-//   };
-
-//   return (
-//     <Wrapper>
-//       <Head>
-//         <script
-//           type="application/ld+json"
-//           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
-//         />
-//       </Head>
-//       <Container>
-//         <Description>
-//           <Heading>{blog.heading}</Heading>
-//           <FetureImg image={blog.image} image_alt={blog.image_alt} />
-//           <div
-//             className="description"
-//             dangerouslySetInnerHTML={{ __html: blog.summary }}
-//           />
-//           <AuthorCard {...author} />
-//         </Description>
-//         <RightSidebar>
-//           <PopularPost popular={popular} />
-//           <LatestPost latest={latest} />
-//           <Ad />
-//         </RightSidebar>
-//       </Container>
-//     </Wrapper>
-//   );
-// };
-
-// export default BlogDetail;
-
-// const Wrapper = styled.div`
-//     background: var(--bg-color);
-//   `;
-
-// const Container = styled.div`
-//     padding: 2rem 0;
-//     margin: 0 3rem;
-//     display: grid;
-//     grid-template-columns: 3fr 1fr;
-//     gap: 3rem;
-
-//     @media (max-width: 768px) {
-//       margin: 0 1rem;
-//       display: flex;
-//       flex-direction: column;
-//     }
-//   `;
-
-// const Description = styled.div`
-//     margin-top: 10rem;
-//     line-height: 1.6;
-//     p,
-//       span {
-//         font-family: "Poppins" !important;
-//         font-size: 1.8rem !important;
-//         color: var(--para) !important;
-//       }
-//          ul li {
-//           list-style-type: disc;
-//           margin-left: 3rem;
-//         }
-//         h1 {
-//           font-size: 32px !important;
-//         }
-//         h2 {
-//           font-size: 28px !important;
-//         }
-//         h3 {
-//           font-size: 24px !important;
-//         }
-
-//     @media (max-width: 768px) {
-//       margin-top: 6rem;
-//     }
-//   `;
-
-// const RightSidebar = styled.aside`
-//     padding: 1rem;
-//     margin-top: 10rem;
-
-//     @media (max-width: 768px) {
-//       margin-top: 1rem;
-//     }
-//   `;
-
-// const Heading = styled.h1`
-//     margin: 1rem;
-//     font-size: 4rem;
-//     text-align: center;
-//     color: var(--primary-color);
-
-//     @media (max-width: 768px) {
-//       font-size: 2.5rem;
-//     }
-//   `;
-
-
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import styled from "styled-components";
@@ -400,22 +256,13 @@ const Heading = styled.h1`
   }
 `;
 
+
 const TOCWrapper = styled.div`
   margin: 2rem 0;
   padding: 2rem;
   background: white;
   box-shadow: 0 8px 4px rgba(0, 0, 0, 0.06);
   font-family: "Poppins", sans-serif;
-  max-height: 400px;
-  overflow-y: auto;
-
-  &::-webkit-scrollbar {
-    width: 6px;
-  }
-
-  &::-webkit-scrollbar-thumb {
-    border-radius: 3px;
-  }
 
   h3 {
     font-size: 2rem;
@@ -438,6 +285,18 @@ const TOCWrapper = styled.div`
     list-style: none;
     padding-left: 0;
     margin: 0;
+
+    max-height: 300px; 
+    overflow-y: auto;  
+
+    &::-webkit-scrollbar {
+      width: 6px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color:#d4af37;
+      border-radius: 3px;
+    }
   }
 
   .toc-flat-list li {
@@ -447,14 +306,13 @@ const TOCWrapper = styled.div`
   }
 
   .toc-flat-list li::before {
-   content: attr(data-symbol);
-   position: absolute;
-   left: 0;
-   top: -0.1rem;
-   font-size: 1.8rem;
-   color: #1a3ea8;
+    content: attr(data-symbol);
+    position: absolute;
+    left: 0;
+    top: -0.1rem;
+    font-size: 1.8rem;
+    color: #1a3ea8;
   }
-
 
   .toc-flat-list li a {
     font-size: 1.8rem;
@@ -476,15 +334,14 @@ const TOCWrapper = styled.div`
     max-height: none;
   }
 `;
-
 const TOCWrapperMobile = styled(TOCWrapper)`
   display: none;
 
-  @media (max-width: 768px) {
+   @media (max-width: 768px) {
     display: block;
-    max-height: 300px;
-    overflow-y: auto;
-    border-radius:16px;
+    max-height: none;   
+    overflow-y: unset;         
+    border-radius: 16px;
 
     &::-webkit-scrollbar {
       width: 6px;
