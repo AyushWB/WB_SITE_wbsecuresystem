aa9b89b6a894c3fb746c316437e3d8f6694a4e8e
Ayush <ayush02.wb@gmail.com>
2025-09-10 12:37:27 +0530

    link issue fix footerrelatedsearch

--- components/miscellaneous/footer/FooterRelatedSearch.js
+++ components/miscellaneous/footer/FooterRelatedSearch.js
@@ -3,53 +3,21 @@ import styled from 'styled-components'
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-function FooterRelatedSearch({city,locality}) {
+function FooterRelatedSearch({ city, locality }) {
     const router = useRouter();
 
     const currentSlug = router.asPath.split('/')[1];
 
     const venue_categories = [
-        {
-            "id": 3,
-            "name": "Banquet Halls",
-            "slug": "banquet-halls"
-        },
-        {
-            "id": 4,
-            "name": "Marriage Gardens",
-            "slug": "marriage-gardens"
-        },
-        {
-            "id": 5,
-            "name": "Wedding Farmhouse",
-            "slug": "wedding-farmhouse"
-        },
-        {
-            "id": 6,
-            "name": "Party Halls",
-            "slug": "party-halls"
-        },
-        {
-            "id": 7,
-            "name": "5 Star Wedding Hotels",
-            "slug": "5-star-wedding-hotels"
-        },
-        {
-            "id": 11,
-            "name": "Small Function Halls",
-            "slug": "small-function-halls"
-        },
-        {
-            "id": 13,
-            "name": "Corporate Events",
-            "slug": "corporate-events"
-        },
-        {
-            "id": 14,
-            "name": "Destination Weddings",
-            "slug": "destination-weddings"
-        }
-    ]
+        { id: 3, name: "Banquet Halls", slug: "banquet-halls" },
+        { id: 4, name: "Marriage Gardens", slug: "marriage-gardens" },
+        { id: 5, name: "Wedding Farmhouse", slug: "wedding-farmhouse" },
+        { id: 6, name: "Party Halls", slug: "party-halls" },
+        { id: 7, name: "5 Star Wedding Hotels", slug: "5-star-wedding-hotels" },
+        { id: 11, name: "Small Function Halls", slug: "small-function-halls" },
+        { id: 13, name: "Corporate Events", slug: "corporate-events" },
+        { id: 14, name: "Destination Weddings", slug: "destination-weddings" },
+    ];
 
     const filteredCategories = venue_categories.filter(cat => cat.slug !== currentSlug);
 
@@ -61,9 +29,18 @@ function FooterRelatedSearch({city,locality}) {
                     <div className="vendors-list">
                         {
                             filteredCategories.map((cat) => {
+                                // 👇 override link target if category is small-function-halls or corporate-events
+                                const targetSlug =
+                                    (cat.slug === "small-function-halls" || cat.slug === "corporate-events")
+                                        ? "banquet-halls"
+                                        : cat.slug;
+
                                 return (
                                     <span key={cat.id}>
-                                        <Link className='vendor-link' href={`/${cat.slug}/${city}/${locality}`}>
+                                        <Link
+                                            className='vendor-link'
+                                            href={`/${targetSlug}/${city}/${locality}`}
+                                        >
                                             {`${cat.name} in ${locality === "all" ? city : locality}`}
                                         </Link>
                                     </span>
@@ -82,26 +59,23 @@ margin-top: 2rem;
 .vendors-heading{
     font-size: 2rem;
     letter-spacing: 1px;
-
 }
 .vendors-container{
     padding: 1rem 0rem;
     .vendors-title{
         font-family: Poppins;
         font-size: 1.8rem;
-        
     }
     .vendors-list{
         padding: 2rem 0rem;
         
         span:not(:last-child)::after {
-        content: "|";
-        padding: 0 10px;
-        opacity: .54;
-        color: black;
-        font-size: 1.5rem;
-  }
-
+            content: "|";
+            padding: 0 10px;
+            opacity: .54;
+            color: black;
+            font-size: 1.5rem;
+        }
 
         .vendor-link{
             line-height: 3rem;
@@ -120,9 +94,6 @@ margin-top: 2rem;
         }
     }
 }
-
 `
-
-
 
 export default FooterRelatedSearch
